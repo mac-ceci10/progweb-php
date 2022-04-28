fd801557dd2ff61034066979dd39778bfd6e7791
mac-ceci10 <escolaeducativa.mac@gmail.com>
2022-04-28 16:31:39 -0300

    Create 05-loops.php
    loops

---
+++ 05-loops.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loops</title>
+</head>
+
+<body>
+    <h1> Loops PHP</h1>
+    <hr>
+
+    <h2> While (enquanto)</h2>
+
+    <?php
+    $i= 1;
+    while($i <=3) {// ou $i <4
+    ?>
+
+    <p> <?=$i?> </p>
+
+    <?php
+    $i++; // atualizar a variável de controle 
+    }
+    ?>  
+
+    <h2> do / while (faça/enquanto)</h2>
+
+<?php
+$j = 1;
+do {
+?>
+    <div style="border: solid 1px"></div>
+    <h3>Exemplo <?=$j?> </h3> 
+    </div>
+
+<?php
+$j++;
+} while($j<=5);
+?>
+
+<h2> for (para) </h2>
+
+<?php
+for ($i=1; $i <=10; $i++){
+?>
+<p> valor de i é <b> <?=$i?> </b> </p>
+
+<?php
+}
+?>
+
+
+</body>
+</html>
